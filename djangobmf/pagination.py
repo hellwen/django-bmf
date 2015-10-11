22b785303eea835acf95f74d356734366dccf2f0
Sebastian Braun <sebastian@elmnt.de>
2015-10-11 21:32:31 +0200

    added missing file

---
+++ djangobmf/pagination.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# ex:set fileencoding=utf-8:
+
+from __future__ import unicode_literals
+
+from rest_framework import pagination
+
+
+class ModulePagination(pagination.PageNumberPagination):
+
+    def get_paginated_response(self, data):
+        return Response({
+            'pagination': {
+                'next': self.get_next_link(),
+                'prev': self.get_previous_link(),
+                'count': self.page.paginator.count,
+            },
+            'items': data,
+        })
