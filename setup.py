cd89d72623449e467f046380067ec058a5341302
Sebastian Braun <sebastian@elmnt.de>
2015-10-15 06:19:10 +0200

    drop support for python 3.3

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ CLASSIFIERS = [
     'Operating System :: OS Independent',
     'Programming Language :: Python',
     'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
