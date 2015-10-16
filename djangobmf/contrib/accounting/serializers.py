b59298fa44862da50e38704f7a757ab588d82aa4
Sebastian Braun <sebastian@elmnt.de>
2015-10-16 02:38:22 +0200

    update serializer

--- djangobmf/contrib/accounting/serializers.py
+++ djangobmf/contrib/accounting/serializers.py
@@ -12,23 +12,33 @@ class AccountSerializer(ModuleSerializer):
     balance_formatted = serializers.SerializerMethodField()
     type_name = serializers.SerializerMethodField()
 
+    class Meta:
+        fields = [
+            'parent', 'number', 'name', 'balance', 'balance_currency',
+            'balance_formatted', 'type_name', 'bmfdetail',
+        ]
+
     def get_balance_formatted(self, obj):
         return '%s' % obj.balance
 
     def get_type_name(self, obj):
         return '%s' % obj.get_type_display()
 
-    class Meta:
-        fields = ['pk', 'balance_formatted', 'type_name', 'bmfdetail']
-
 
 class TransactionSerializer(ModuleSerializer):
-    pass
+    class Meta:
+        fields = ['text', 'project', 'bmfdetail']
 
 
 class TransactionItemSerializer(ModuleSerializer):
     transaction_name = serializers.SerializerMethodField()
     account_name = serializers.SerializerMethodField()
+
+    class Meta:
+        fields = [
+            'credit', 'amount', 'amount_currency', 'account_name',
+            'transaction_name', 'bmfdetail',
+        ]
 
     def get_account_name(self, obj):
         return '%s' % obj.account
