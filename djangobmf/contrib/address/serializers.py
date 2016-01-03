7ac95f189b2569b58006b7173b922a3749b3c1ff
Sebastian Braun <sebastian@elmnt.de>
2016-01-03 13:57:25 +0100

    bugfix

--- djangobmf/contrib/address/serializers.py
+++ djangobmf/contrib/address/serializers.py
@@ -5,6 +5,26 @@ from __future__ import unicode_literals
 
 from djangobmf.serializers import ModuleSerializer
 
+from rest_framework import serializers
+
 
 class AddressSerializer(ModuleSerializer):
-    pass
+    name = serializers.SerializerMethodField()
+    customer_name = serializers.SerializerMethodField()
+
+    class Meta:
+        fields = (
+            'name',
+            'customer_name',
+            'is_active',
+            'is_billing',
+            'is_shipping',
+            'default_billing',
+            'default_shipping',
+        )
+
+    def get_name(self, obj):
+        return '%s' % obj
+
+    def get_customer_name(self, obj):
+        return '%s' % obj.customer
--- djangobmf/contrib/customer/serializers.py
+++ djangobmf/contrib/customer/serializers.py
@@ -5,6 +5,22 @@ from __future__ import unicode_literals
 
 from djangobmf.serializers import ModuleSerializer
 
+from rest_framework import serializers
+
 
 class CustomerSerializer(ModuleSerializer):
-    pass
+    username = serializers.ReadOnlyField(source='user.username')
+    employee = serializers.ReadOnlyField(source='employee_at.name')
+
+    class Meta:
+        fields = (
+            'number',
+            'name',
+            'username',
+            'employee',
+            'employee_at',
+            'is_company',
+            'is_active',
+            'is_customer',
+            'is_supplier',
+        )
