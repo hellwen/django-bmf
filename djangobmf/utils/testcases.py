5f63c40d8fae6b085684b61726d2fe9ac8b24114
Sebastian Braun <sebastian@elmnt.de>
2016-01-28 11:57:41 +0100

    update tests

--- djangobmf/utils/testcases.py
+++ djangobmf/utils/testcases.py
@@ -177,9 +177,26 @@ class ModuleTestFactory(SuperuserMixin, BaseTestCase):
 
             for obj in model.objects.all():
                 url = reverse('%s:api-detail' % settings.APP_LABEL, kwargs={
-                    'app': model._meta.app_label,
-                    'model': model._meta.model_name,
+                    'app': obj._meta.app_label,
+                    'model': obj._meta.model_name,
                     'pk': obj.pk,
+                })
+                response = self.client.get(url, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+                self.assertTrue(response.status_code in [200])
+
+    def test_module_api_related(self):
+        self.appconf = [app for app in apps.get_app_configs() if isinstance(app, self.app)][0]
+
+        for relation in self.appconf.bmf_config.bmf_relations:
+            if relation._model not in self.models:
+                continue
+
+            for obj in relation._model.objects.all():
+                url = reverse('%s:api-related' % settings.APP_LABEL, kwargs={
+                    'app': obj._meta.app_label,
+                    'model': obj._meta.model_name,
+                    'pk': obj.pk,
+                    'field': relation.slug,
                 })
                 response = self.client.get(url, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
                 self.assertTrue(response.status_code in [200])
