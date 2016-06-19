76c79836bcd9d598e021d80929855b1515c496df
Sebastian Braun <sebastian@elmnt.de>
2016-06-19 12:13:33 +0200

    added relation and serializers for ProductTax, updated templates

--- djangobmf/contrib/product/serializers.py
+++ djangobmf/contrib/product/serializers.py
@@ -5,6 +5,27 @@ from __future__ import unicode_literals
 
 from djangobmf.serializers import ModuleSerializer
 
+from rest_framework import serializers
+
 
 class ProductSerializer(ModuleSerializer):
-    pass
+    class Meta:
+        fields = (
+            'name',
+            'code',
+            'type',
+            'can_sold',
+            'can_purchased',
+            'price',
+        )
+
+
+class ProductTaxSerializer(ModuleSerializer):
+    name = serializers.ReadOnlyField(source='tax.name')
+
+    class Meta:
+        fields = (
+            'name',
+            'tax',
+            'included',
+        )
